244eaa09864f06d84a8f08d3a5e1a795c32f1827
Migue <Migue@DESKTOP-NV1H4VI.lan>
2019-10-31 15:38:51 +0000

    Cliente envia imagens
    Alterei o sendImages para enviar byte arrays para o algoritmo e guardar
    a resposta em resultados que vai ser notificado para os observadores.

--- LogoSearcher/src/client/Algorithm.java
+++ LogoSearcher/src/client/Algorithm.java
@@ -3,12 +3,13 @@ package client;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
 
 public class Algorithm {
 
-	public boolean procura(BufferedImage imagem, BufferedImage subimagem) {
+	public static boolean procuraSub(BufferedImage imagem, BufferedImage subimagem) {
 		
 		for(int i=0; i!= imagem.getWidth();i++) {
 			for(int j=0; j!=imagem.getHeight();j++) {
@@ -34,11 +35,15 @@ public class Algorithm {
 		return false;
 	}
 	
+	public static ArrayList<Integer> procura(byte[] imagem, byte[] subimagem) {
+		return null;
+	}
+	
 	public static void main(String[] args) {
-		Algorithm a = new Algorithm();
 		 try {
-			BufferedImage imagem = ImageIO.read(new File("C:\\Users\\amges\\Downloads\\img\\img\\out\\image1_1"));
-			BufferedImage subimagem = ImageIO.read(new File("image1_1"));
+			BufferedImage imagem = ImageIO.read(new File("out/image1_1.png"));
+			BufferedImage subimagem = ImageIO.read(new File("Superman.png"));
+			procuraSub(imagem, subimagem);
 		 } catch (IOException e) {
 		
 			e.printStackTrace();
--- LogoSearcher/src/client/Client.java
+++ LogoSearcher/src/client/Client.java
@@ -1,22 +1,40 @@
 package client;
 
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Observable;
 
-public class Client extends Observable{
+import javax.imageio.ImageIO;
+
+public class Client extends Observable {
 	
-	void sendImages(Object o) {
-		
+	public void sendImages(BufferedImage imagem, BufferedImage subimagem) {
+		byte[] img = convertToByteArray(imagem);
+		byte[] subimg = convertToByteArray(subimagem);
+		ArrayList<Integer> results = Algorithm.procura(img, subimg);
+		notifyObservers(results);
 	}
-	
-	void serverAnswers() {
-		//Recebe os resultados
+
+	private byte[] convertToByteArray(BufferedImage img) {
+		try {
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			ImageIO.write(img, "png", baos);
+			baos.flush();
+			byte[] bImg = baos.toByteArray();
+			baos.close();
+			return bImg;
+		} catch (IOException e) {
+		}
+		return null;
 	}
-	
+
 	public static void main(String[] args) {
 		Client c = new Client();
 		GUI window = new GUI(c);
 		c.addObserver(window);
 		window.open();
 	}
-	
+
 }
