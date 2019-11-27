f984f463bd17b6a4dbc87e9080c6403b7702ff50
Mik322 <mchsa@iscte-iul.pt>
2019-11-27 16:28:37 +0000

    Client draws Image

--- LogoSearcher/src/client/Client.java
+++ LogoSearcher/src/client/Client.java
@@ -4,9 +4,11 @@ import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.Point;
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.InetAddress;
@@ -24,16 +26,16 @@ import streamedobjects.Job;
 public class Client extends Observable {
 	private static String OUTPUT_NAME = "OUT";
 	private File file;
-	
+
 	private final int PORTO;
 	private final String endereco;
-	
+
 	private ObjectInputStream in;
 	private ObjectOutputStream out;
 	private Socket socket;
-	
+
 	private static final int CLIENT = 1;
-	
+
 	private void runClient() {
 		try {
 			doConections();
@@ -49,57 +51,61 @@ public class Client extends Observable {
 			e.printStackTrace();
 		}
 	}
-	
+
 	private void doConections() throws IOException {
 		InetAddress endereco = InetAddress.getByName(null);
 		socket = new Socket(endereco, PORTO);
 		in = new ObjectInputStream(socket.getInputStream());
 		out = new ObjectOutputStream(socket.getOutputStream());
 	}
-	
+
 	public void sendImages(File[] imagensDir, BufferedImage subimagem, List<String> types) {
 		byte[] subimg = convertToByteArray(subimagem);
-		int n = 1;
 		ArrayList<byte[]> imgs = new ArrayList<>();
-		
-		for (File f:imagensDir) {
+
+		for (File f : imagensDir) {
 			try {
 				BufferedImage imagem = ImageIO.read(f);
 				byte[] img = convertToByteArray(imagem);
 				imgs.add(img);
-			} catch (Exception e) {}
+			} catch (Exception e) {
+			}
 		}
 		try {
 			out.writeObject(new Job(imgs, subimg, types));
 			out.flush();
-		} catch (IOException e) {}
-			try {
-				@SuppressWarnings("unchecked")
-				HashMap<byte[], ArrayList<Point[]>> results = (HashMap<byte[], ArrayList<Point[]>>) in.readObject();
-			} catch (ClassNotFoundException e) {
-			} catch (IOException e) {}
-			
-			/*if (results.size()!=0) {
-				drawImage(results, imagem);
-				n++;
-			}*/
-			
-			/*String fileName = OUTPUT_NAME+"\\out" + n + ".png";
-			try {
-				ImageIO.write(imagem, "png", new File(fileName));
-			} catch (IOException e) {
-			}*/
-			
+		} catch (IOException e) {
+		}
+		receiveResults();
+	}
+
+	private void receiveResults() {
+		int n = 0;
+		try {
+			@SuppressWarnings("unchecked")
+			HashMap<byte[], ArrayList<Point[]>> results = (HashMap<byte[], ArrayList<Point[]>>) in.readObject();
+			for (byte[] i : results.keySet()) {
+				if (results.get(i).size() != 0) {
+					BufferedImage img = convertToImage(i);
+					drawImage(results.get(i), img);
+					String fileName = OUTPUT_NAME + "\\out" + ++n + ".png";
+					ImageIO.write(img, "png", new File(fileName));
+				}
+			}
+		} catch (ClassNotFoundException e) {
+		} catch (IOException e) {
+		}
 		file = new File(OUTPUT_NAME);
 		setChanged();
 		notifyObservers(file);
 	}
 
 	private void drawImage(ArrayList<Point[]> results, BufferedImage imagem) {
-		for (Point[] p: results) {
+		for (Point[] p : results) {
 			Graphics2D g2d = imagem.createGraphics();
 			g2d.setColor(Color.RED);
-			g2d.drawRect((int)p[0].getX(), (int)p[0].getY(), (int)(p[0].getX()-p[1].getX()), (int)(p[0].getY()-p[1].getY()));
+			g2d.drawRect((int) p[0].getX(), (int) p[0].getY(), (int) (p[0].getX() - p[1].getX()),
+					(int) (p[0].getY() - p[1].getY()));
 			g2d.dispose();
 		}
 	}
@@ -116,7 +122,17 @@ public class Client extends Observable {
 		}
 		return null;
 	}
-	
+
+	private BufferedImage convertToImage(byte[] img) {
+		try {
+			InputStream in = new ByteArrayInputStream(img);
+			BufferedImage bImageFromConvert = ImageIO.read(in);
+			return bImageFromConvert;
+		} catch (Exception e) {
+		}
+		return null;
+	}
+
 	public Client(String endereco, int PORTO) {
 		this.endereco = endereco;
 		this.PORTO = PORTO;
